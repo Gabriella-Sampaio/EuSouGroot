30d1abb46affc2ced58769e8a579bf60efd281d4
Gabriella-Sampaio <dixxgabriellasam@gmail.com>
2025-05-23 22:09:33 -0300

    Inicio while

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+string pergunta = "",
+       resposta = "Eu sou o Groot";
+
+Console.WriteLine("Converse com o Groot");
+
+while (pergunta != "TCHAU")
+{
+    Console.Write("Pergunta: ");
+    pergunta = Console.ReadLine()!.ToUpper();
+
+    Console.Write($"Resposta: ");
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"{resposta}\n");
+    Console.ResetColor();
+}
+
+Console.WriteLine("Boa conversa, tchau");
